f66b091286d66c1eedfd977d016560d15da9d7a1
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-21 20:15:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ require_relative 'models/puppy'
   end
 
   post '/puppy' do
-   @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+   @puppy = Puppy.new(params[':name'], params[':breed'], params[':age'])
    erb :display_puppy
  end
 end
